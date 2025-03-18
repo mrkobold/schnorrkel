58fcad5bcbebd1ef6aa1e48043bf21284cf5cbcb
mrkobold <kovacsboldizsar@gmail.com>
2025-03-18 16:12:05 +0200

    Nasty bug, these were overriding the real i values

--- src/sign.rs
+++ src/sign.rs
@@ -282,10 +282,10 @@ impl PublicKey {
             // res = 2 done
             let R_compressed = R.compress();
             if R_compressed == signature.R {
-                update_kobold_account_handle(444, [0u64; 15]);
+                update_kobold_account_handle(1000, [0u64; 15]);
                 return 4u8; // Signature Ok
             } else {
-                update_kobold_account_handle(333, [0u64; 15]);
+                update_kobold_account_handle(1001, [0u64; 15]);
                 return 3u8; // Signature Not Ok
             }
         }
